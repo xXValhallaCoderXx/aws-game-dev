d2a0ada97b6fbc27a0c72f61a90f854b80a941ee
Renate <renate.gouveia@gmail.com>
2024-11-23 20:29:48 +0800

    Added water base layer

--- src/scenes/HomeMap.ts
+++ src/scenes/HomeMap.ts
@@ -2,6 +2,8 @@ import { BaseScene } from "./BaseScene";
 import { ESCENE_KEYS } from "../shared/scene-keys";
 
 export class HomeMap extends BaseScene {
+  private waterLayer?: Phaser.Tilemaps.TilemapLayer | null;
+
   constructor() {
     super(ESCENE_KEYS.CAMERA);
   }
@@ -10,23 +12,55 @@ export class HomeMap extends BaseScene {
     super.preload();
     this.load.tilemapTiledJSON("home-map", "maps/home-map.json");
     this.load.image("terrain-village-1", "tilesets/terrain-village-1.png");
+    this.load.image("water-blank", "tilesets/water-blank.png");
   }
 
   protected createMap(): void {
     this.map = this.make.tilemap({ key: "home-map" });
 
-    // Add tilesets
     const terrainVillage1Tileset = this.map.addTilesetImage(
       "terrain-village-1",
       "terrain-village-1"
     );
 
-    if (!terrainVillage1Tileset) {
+    const waterBlankTileset = this.map.addTilesetImage(
+      "water-blank",
+      "water-blank"
+    );
+
+    if (!terrainVillage1Tileset || !waterBlankTileset) {
       throw new Error("Failed to load terrain tileset");
     }
 
-    // Create layers
     this.map.createLayer("GrassBaseLayer", terrainVillage1Tileset, 0, 0);
     this.map.createLayer("GrassAccessoriesLayer", terrainVillage1Tileset, 0, 0);
+
+    this.waterLayer = this.map.createLayer(
+      "WaterBaseLayer",
+      waterBlankTileset,
+      0,
+      0
+    );
+
+    if (this.waterLayer) {
+      this.waterLayer.setCollisionByProperty({ collides: true });
+    }
+  }
+
+  create() {
+    // First create the map
+    this.createMap();
+
+    // Then call parent's create which will handle player creation and camera setup
+    super.create();
+
+    // Finally set up the water collisions
+    this.setupCollisions();
+  }
+
+  private setupCollisions(): void {
+    if (this.waterLayer && this.player) {
+      this.physics.add.collider(this.player, this.waterLayer);
+    }
   }
 }
