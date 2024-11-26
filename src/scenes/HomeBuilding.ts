d44df82eb9dd1637a2865d70529d80456816d25e
renate-hq <renate@hq.xyz>
2024-11-26 22:58:25 +0800

    Working on item

--- src/scenes/HomeBuilding.ts
+++ src/scenes/HomeBuilding.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Scene } from "phaser";
+import { BaseScene } from "./BaseScene";
 import { ESCENE_KEYS } from "../shared/scene-keys";
 
-export class HomeBuilding extends Scene {
+export class HomeBuilding extends BaseScene {
   constructor() {
     super(ESCENE_KEYS.HOME_HOUSE);
   }
@@ -11,7 +11,67 @@ export class HomeBuilding extends Scene {
     console.log("HOME HOUSE");
   }
 
-  preload() {}
+  preload() {
+    super.preload();
+    this.load.tilemapTiledJSON("building-map", "maps/home-building-map.json");
+    this.load.image(
+      "building-interior-floors",
+      "tilesets/building-interior-floors.png"
+    );
+    this.load.image(
+      "building-interior-mixed",
+      "tilesets/building-interior-mixed.png"
+    );
+    this.load.image(
+      "building-interior-objects",
+      "tilesets/building-interior-objects.png"
+    );
+    this.load.image(
+      "building-interior-walls",
+      "tilesets/building-interior-walls.png"
+    );
+    this.load.image(
+      "building-interior-windows",
+      "tilesets/building-interior-windows.png"
+    );
 
-  create() {}
+    this.load.audio("backgroundMusic", "sounds/main-bgm.mp3");
+  }
+
+  create() {
+    // First create the map
+    this.createMap();
+
+    // Then call parent's create which will handle player creation and camera setup
+    super.create();
+  }
+
+  protected createMap(): void {
+    this.map = this.make.tilemap({ key: "building-map" });
+
+    const interiorFloorsTileset = this.map.addTilesetImage(
+      "building-interior-floors",
+      "building-interior-floors"
+    );
+
+    const interiorWallsTileset = this.map.addTilesetImage(
+      "building-interior-walls",
+      "building-interior-walls"
+    );
+
+    if (!interiorFloorsTileset || !interiorWallsTileset) {
+      throw new Error("Failed to load interior floors tileset");
+    }
+
+    this.map.createLayer("FloorsBaseLayer", interiorFloorsTileset, 0, 0);
+    this.map.createLayer("WallBaseLayer", interiorWallsTileset, 0, 0);
+
+    // if (this.waterLayer) {
+    //   this.waterLayer.setCollisionByProperty({ collides: true });
+    // }
+
+    // if (this.waterAnimatedLayer) {
+    //   this.waterAnimatedLayer.setCollisionByProperty({ collides: true });
+    // }
+  }
 }
