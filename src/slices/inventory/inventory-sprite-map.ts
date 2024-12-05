095363ca33cf893c97d1bbf27cd68427b30d31a6
Renate <renate.gouveia@gmail.com>
2024-12-05 22:10:26 +0800

    Item is being added to the toolbar correctly

--- src/slices/inventory/inventory-sprite-map.ts
+++ src/slices/inventory/inventory-sprite-map.ts
@@ -1,7 +1,13 @@
-import { EFarmingCrops } from "../farming/farming.interface";
+import {
+  EFarmingCrops,
+  EFarmingCropYields,
+} from "../farming/farming.interface";
 
 export const INVENTORY_SPRITE_MAPPING = {
   [EFarmingCrops.CARROT]: 0,
   [EFarmingCrops.RADISH]: 1,
   [EFarmingCrops.CAULIFLOWER]: 2,
+  [EFarmingCropYields.CARROT]: 0,
+  [EFarmingCropYields.RADISH]: 1,
+  [EFarmingCropYields.CAULIFLOWER]: 2,
 };
