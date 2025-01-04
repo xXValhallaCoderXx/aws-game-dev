c23427c0e7e7015b0ff761ba757216a3e0ad28cd
Renate <renate.gouveia@gmail.com>
2025-01-04 17:04:27 +0800

    Hook up inventory panel

--- src/shared/components/organisms/InventoryPanel/InventoryPanel.tsx
+++ src/shared/components/organisms/InventoryPanel/InventoryPanel.tsx
@@ -2,27 +2,36 @@ import styles from "./inventory-panel.module.css";
 import { RootState } from "@/shared/services/redux-store.service";
 import { SpriteIcon } from "../../atoms/SpriteIcon";
 import { useSelector } from "react-redux";
+import { PhaserEventBus } from "@/shared/services/phaser-event.service";
 import { INVENTORY_SPRITE_MAPPING } from "@/slices/inventory/inventory-sprite-map";
 
 const InventoryPanel = () => {
   const { items } = useSelector((state: RootState) => state.inventory);
 
-  const handleOnClickItem = () => console.log("CLick");
+  const handleOnClickItem = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const index = Number(event.currentTarget.id);
+    const item = items[index];
+
+    PhaserEventBus.emit("inventory:seedSelected", item?.id);
+  };
 
   // Create an array of 18 slots (2 rows x 9 columns)
   const slots = Array.from({ length: 18 }, (_, index) => items[index] || null);
-  console.log("SLOTS: ", slots);
 
   return (
     <div>
       <div className={styles.topSection}>
         <div className={styles.itemsContainer}>
           {slots.map((item, index) => (
-            <button key={index} className={styles.toolbarButton}>
+            <button
+              key={index}
+              id={String(index)}
+              className={styles.toolbarButton}
+              onClick={handleOnClickItem}
+            >
               <SpriteIcon
                 data={item}
                 spriteSheet={item?.category ?? "seeds"}
-                onClick={item ? handleOnClickItem : undefined}
                 iconIndex={
                   item
                     ? INVENTORY_SPRITE_MAPPING[
